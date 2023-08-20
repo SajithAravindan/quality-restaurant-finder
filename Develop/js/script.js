316a74185073f3c543b962f8371b2f27feb11cfe
Sajith Aravindan <sajitharavindan11@gmail.com>
2023-08-20 00:22:07 -0400

    added stars for rating

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -14,11 +14,12 @@ let favoritesArea = document.querySelector('#favorites');
 function init() {
     //Event Handler for Button Click
     userFormEl.addEventListener('submit', SearchSubmitHandler);// Search Button   
-    btnAlert.addEventListener('click', CloseAlert);// Search Button 
+    btnAlert.addEventListener('click', CloseAlert);// error close Button 
     //Empty Pre existing data
     divContainerMain.textContent = "";
 }
 
+//close the error alert
 var CloseAlert =()=>document.getElementById('divAlert').style.display="none";
 
 //Function called when search button is clicked
@@ -34,7 +35,7 @@ var SearchSubmitHandler = function (event) {
         getUserRepos(strUserSearch);//get restaurant data        
         strCityName.textContent = '';
     } else {
-        document.getElementById('divAlert').style.display="block";
+        document.getElementById('divAlert').style.display="block";//display error msg
     }
 };
 
@@ -97,7 +98,6 @@ function getRestaurantData(intULocId) {
         .then(function (response) {
             if (response.ok) {
                 response.json().then(function (data) {
-                    console.log(data);
                     DisplayRestaurantData(data);
                 });
             } else {
@@ -144,7 +144,7 @@ function DisplayRestaurantData(objRestuarantAPIJson) {
         strFLContnet = ''
         if (objRestuarantAPIJson.results.data[i].description != '' && objRestuarantAPIJson.results.data[i].description != null)
             strFLContnet = objRestuarantAPIJson.results.data[i].description + '<br /><br />';
-        strFLContnet = strFLContnet + '<b>Rating:</b> ' + objRestuarantAPIJson.results.data[i].rating + ',  <b>Ranking :</b> ' + objRestuarantAPIJson.results.data[i].ranking +
+        strFLContnet = strFLContnet + '<b>Rating:</b> ' + objRestuarantAPIJson.results.data[i].rating + '<label id="Stars-' + i + '"></label>,  <b>Ranking :</b> ' + objRestuarantAPIJson.results.data[i].ranking +
             ',  <b>Open/Close :</b> ' + objRestuarantAPIJson.results.data[i].open_now_text + ', ' + objRestuarantAPIJson.results.data[i].price_level +
             ',  <b>Distance :</b> <label id="DD-' + i + '"></label>';
         divCardBlockContentTemp.innerHTML = strFLContnet;
@@ -192,7 +192,8 @@ function DisplayRestaurantData(objRestuarantAPIJson) {
         divCardBlock.append(divCardBlockBody);
         divContainerMain.append(divCardBlock);//add card to container
 
-        fetchDistance(objRestuarantAPIJson.results.data[i].address, i);
+        fetchDistance(objRestuarantAPIJson.results.data[i].address, i);//Get Distance & Duration
+        getStars(objRestuarantAPIJson.results.data[i].rating,i);//Get Rating Stars
     }
     // Runs after looping in order to set event listeners to button elements
     storeFavorites();
@@ -208,7 +209,7 @@ function getInnerContents(objContents) {
     return strInnerContents;
 }
 
-//Funtion returns distance & duration from a origin to destination.
+//Function returns distance & duration from a origin to destination.
 //destination and loop id as parameters
 function fetchDistance(strdestinationlat, id) {    
     var ielement = "DD-"
@@ -292,27 +293,28 @@ function getRestaurants() {
     }
 }
 
+//Function to display Rating stars
 function getStars(rating,id) {
     var starDisplay = document.getElementById('Stars-'+id);
-    var strResultsStars="working";
+    let strResultsStars="working";
     // Round to nearest half
     rating = Math.round(rating * 2) / 2;
     let output = [];
-    alert(1);
+   
     // Append all the filled whole stars
     for (var y = rating; y >= 1; y--)
       output.push('<i class="fa fa-star" aria-hidden="true" style="color: gold;"></i>&nbsp;');
   
     // If there is a half a star, append it
-    if (i == .5) output.push('<i class="fa fa-star-half-o" aria-hidden="true" style="color: gold;"></i>&nbsp;');
+    if (y == .5) output.push('<i class="fa fa-star-half-o" aria-hidden="true" style="color: gold;"></i>&nbsp;');
   
     // Fill the empty stars
     for (let x = (5 - rating); x >= 1; x--)
       output.push('<i class="fa fa-star-o" aria-hidden="true" style="color: gold;"></i>&nbsp;');
   
       strResultsStars = output.join('');
-      alert(strResultsStars);
-    return output.join('');
+      
+      starDisplay.innerHTML = strResultsStars;
   
   }
 
